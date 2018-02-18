70e6eb07a0034e2de025a300a7843e6a77f8b949
paziolka <ola.pazio@gmail.com>
2018-02-18 04:48:50 +0100

    Add 2 components: FakePromoChallenge and FakeNewsChecklist

---
+++ src/js/components/Challenges/FakePromoChallenge/index.js
@@ -0,0 +1,12 @@
+import Preact from 'preact'
+import './index.sass'
+
+class FakePromoChallenge extends Preact.Component {
+  render() {
+    return (
+      <div>FakePromoChallenge</div>
+    )
+  }
+}
+
+export default FakePromoChallenge
--- src/js/views/Challenge.js
+++ src/js/views/Challenge.js
@@ -2,13 +2,19 @@ import Preact from 'preact'
 import { connect } from 'react-redux'
 import StoryChallenge from '../components/Challenges/StoryChallenge'
 import CommentModeration from '../components/Challenges/CommentModeration'
+import FakeNewsChecklist from '../components/Challenges/FakeNewsChecklist'
+import FakePromoChallenge from '../components/Challenges/FakePromoChallenge'
 
 const Challenge = ({ type }) => {
   switch (type) {
     case 'comment_moderation':
       return <CommentModeration />
-    case 'type2':
+    case 'story_challenge':
       return <StoryChallenge />
+    case 'fake_news_checklist':
+      return <FakeNewsChecklist />
+    case 'fake_promo_challenge':
+      return <FakePromoChallenge />
     default:
       return <p>No matching challenge for type: {type}</p>
   }
