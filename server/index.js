11c8a1e5243e7aee1a3bcc9a6151469a1d273a73
Bartosz Legięć <bartosz@legiec.eu>
2018-02-25 22:55:09 +0100

    Made react router work after reload

--- server/index.js
+++ server/index.js
@@ -1,31 +1,31 @@
 /* eslint-disable import/no-commonjs */
-const jsonServer = require("json-server");
-const path = require("path");
-const express = require("express");
+const jsonServer = require('json-server');
+const path = require('path');
+const express = require('express');
 
-const { questions } = require("./db.json");
+const { questions } = require('./db.json');
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use("/api", middlewares);
-server.use("/api", jsonServer.bodyParser);
+server.use('/api', middlewares);
+server.use('/api', jsonServer.bodyParser);
 
 // Add custom routes before JSON Server router
-server.post("/api/echo", (req, res) => {
+server.post('/api/echo', (req, res) => {
   res.jsonp(req.body);
 });
 
-server.get("/api/question", (req, res) => {
+server.get('/api/question', (req, res) => {
   res.jsonp(questions[Math.floor(questions.length * Math.random())]);
 });
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use("/api", (req, res, next) => {
-  if (req.method === "POST") {
+server.use('/api', (req, res, next) => {
+  if (req.method === 'POST') {
     req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
@@ -33,9 +33,14 @@ server.use("/api", (req, res, next) => {
 });
 
 // Use default router
-server.use("/api", router);
+server.use('/api', router);
 
-server.use(express.static("dist"));
+server.use(express.static('dist'));
+
+server.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '../dist', 'index.html'))
+})
+
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log('JSON Server is running');
 });
